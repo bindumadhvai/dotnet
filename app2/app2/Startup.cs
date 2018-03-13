d02cbb672b38dc12a19d9901f91d42d1dd9acdd0
bindumadhvai <bindumadhavi1996@gmail.com>
2018-03-13 15:41:24 +0530

    first commit

---
+++ app2/app2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(app2.Startup))]
+namespace app2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
